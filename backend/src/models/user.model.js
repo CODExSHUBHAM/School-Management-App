401834efdd90ad236cf9630644307a9b98b2c8d1
CODExSHUBHAM <shubham.nh10@gmail.com>
2024-06-24 14:52:04 +0530

    Add: Added user data model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose"
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            enum: ["ADMIN", "EMPLOYEE", "TEACHER", "STUDENT"],
+            required: true
+        }
+    },
+    { timestamps: true }
+)
+
+export const User = model("User", userSchema)
